beedba889bb1f6422beb1296c7c1703ccb183f43
benps <benpsnook@gmail.com>
2025-03-18 15:39:29 -0500

    Replaced test graph data with real data. Made draw_graph() a little more concise

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import matplotlib.pyplot as plt
 
 def create_weighted_graph():
     G = nx.Graph()
-    G.add_edge('A', 'B', weight=4)
-    G.add_edge('A', 'C', weight=2)
-    G.add_edge('B', 'C', weight=1)
-    G.add_edge('B', 'D', weight=5)
-    G.add_edge('C', 'D', weight=8)
-    G.add_edge('C', 'E', weight=10)
-    G.add_edge('D', 'E', weight=2)
-    G.add_edge('D', 'F', weight=6)
-    G.add_edge('E', 'F', weight=3)
+    G.add_edge('A', 'B', weight=22)
+    G.add_edge('A', 'C', weight=9)
+    G.add_edge('A', 'D', weight=12)
+    G.add_edge('B', 'C', weight=35)
+    G.add_edge('B', 'F', weight=36)
+    G.add_edge('B', 'H', weight=34)
+    G.add_edge('C', 'D', weight=4)
+    G.add_edge('C', 'E', weight=65)
+    G.add_edge('C', 'F', weight=42)
+    G.add_edge('D', 'E', weight=33)
+    G.add_edge('D', 'I', weight=30)
+    G.add_edge('E', 'F', weight=18)
+    G.add_edge('E', 'G', weight=23)
+    G.add_edge('F', 'G', weight=39)
+    G.add_edge('F', 'H', weight=24)
+    G.add_edge('G', 'H', weight=25)
+    G.add_edge('G', 'I', weight=21)
+    G.add_edge('H', 'I', weight=19)
     return G
 
 def dijkstra_shortest_path_tree(G, start_node):
@@ -25,25 +34,24 @@ def minimum_spanning_tree(G):
     return mst
 
 def draw_graph(G, path, title):
+    pos = nx.spring_layout(G)
+
     if title == "Shortest Path Tree":
-        pos = nx.spring_layout(G)
         plt.figure(figsize=(10, 8))
         
         # original graph
-        nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=10)
+        nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=500, font_size=10)
         labels = nx.get_edge_attributes(G, 'weight')
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
         
         # shortest path tree
         edges_in_path = [(path[node][i], path[node][i+1]) for node in path for i in range(len(path[node])-1)]
-        nx.draw_networkx_edges(G, pos, edgelist=edges_in_path, edge_color='r', width=2)
-        
-        plt.title("Shortest Path Tree using Dijkstra's Algorithm")
-        plt.show()
+        nx.draw_networkx_edges(G, pos, edgelist=edges_in_path, edge_color='red', width=2)
+
     else:
-        pos = nx.spring_layout(G)
         plt.figure(figsize=(10, 5))
 
+        # original graph
         plt.subplot(121)
         nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
         labels = nx.get_edge_attributes(G, 'weight')
@@ -52,12 +60,12 @@ def draw_graph(G, path, title):
 
         # minimum spanning tree
         plt.subplot(122)
-        nx.draw(mst, pos, with_labels=True, node_color='lightgreen', edge_color='blue')
+        nx.draw(mst, pos, with_labels=True, node_color='lightblue', edge_color='red')
         labels = nx.get_edge_attributes(mst, 'weight')
         nx.draw_networkx_edge_labels(mst, pos, edge_labels=labels)
-        plt.title("Minimum Spanning Tree")
 
-        plt.show()
+    plt.title(title)
+    plt.show()
 
 
 G = create_weighted_graph()
